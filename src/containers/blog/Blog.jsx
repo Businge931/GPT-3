e169641ddc4e4e8b673d4a077278f85f2610f2f6
Businge Bisanga <jimmybusinge9@gmail.com>
2022-11-28 22:21:24 +0300

    fixing the blog images

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -13,31 +13,31 @@ const Blog = () => (
     <div className="gpt3__blog-container">
       <div className="gpt3__blog-container_groupA">
         <Article
-          imgUrl={blog01}
+          imageUrl={blog01}
           date="Sep 26, 2021"
-          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
         />
       </div>
       <div className="gpt3__blog-container_groupB">
         <Article
-          imgUrl={blog02}
+          imageUrl={blog02}
           date="Sep 26, 2021"
-          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
         />
         <Article
-          imgUrl={blog03}
+          imageUrl={blog03}
           date="Sep 26, 2021"
-          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
         />
         <Article
-          imgUrl={blog04}
+          imageUrl={blog04}
           date="Sep 26, 2021"
-          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
         />
         <Article
-          imgUrl={blog05}
+          imageUrl={blog05}
           date="Sep 26, 2021"
-          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
         />
       </div>
     </div>
